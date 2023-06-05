ec54d219990c73ebcecdc5b57c4b8894b6e0ea02
Julian Criado <pjcriadop@ufpso.edu.co>
2023-06-05 13:11:41 -0500

    segundo commit

--- tablas/src/App.vue
+++ tablas/src/App.vue
@@ -17,11 +17,25 @@ const borrarTarjeta = (index) =>{
 console.log('KILL to', index)
 users.value.splice (index,1)
 console.log(users)
+
+}
+
+let name,mail
+
+const addUser = () =>{
+    const newUser = {
+        name: name,
+        mail: mail
+    }
+    users.value.unshift (newUser)
 }
 
 </script>
 
 <template>
+    <input type="text" v-model="name" placeholder="Escriba su nombre" class="border rounded">
+    <input type="text" v-model="mail" placeholder="Escriba su correo" class="border rounded">
+    <button class="bg-green-200 px-2 py-1 rounded mt-2" @click="addUser">Añadir</button>
 <CardComponent v-for="(user, index) in users" 
 :key="index" :name="user.name" :mail="user.mail" :index="index"
  @borrarTarjeta="borrarTarjeta"/>
