d664084ee02db32aaac0b349d112e4305ccfe88c
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2023-06-05 12:56:31 -0500

    primer commit

---
+++ tablas/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref} from 'vue';
+import CardComponent from '@/components/CardComponent.vue';
+
+const users = ref([
+    {
+        name: 'Juana de arco',
+        mail: 'Juanita@casialgo.com',
+    },
+    {
+        name: 'Rey arturo',
+        mail: 'Derrocado@eldiablo.com',
+    },
+])
+
+const borrarTarjeta = (index) =>{
+console.log('KILL to', index)
+users.value.splice (index,1)
+console.log(users)
+}
+
+</script>
+
+<template>
+<CardComponent v-for="(user, index) in users" 
+:key="index" :name="user.name" :mail="user.mail" :index="index"
+ @borrarTarjeta="borrarTarjeta"/>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ tablas/src/components/CardComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+const borrarTarjeta = (index) =>{
+    console.log('Borrar tarjeta',index)
+    emit('borrarTarjeta',index)
+}
+
+const emit = defineEmits(['borrarTarjeta'])
+
+const props = defineProps(
+    {
+        name:{
+            type:String,
+            default: 'No name'
+        },
+        mail:{
+            type:String,
+            default: 'No email'
+        },
+        index:{
+            type:Number,
+        }
+    }
+)
+</script>
+<template>
+    <div class="w-60 rounded shadow overflow-hidden">
+        <div class="inline-block align-top">
+            <img src="https://picsum.photos/40" alt="">
+        </div>
+        <div class="inline-block pl-2">
+            <h2 class="text-md font-semibold">{{ props.name}}</h2>
+            <p class="text-xs">{{ props.mail }}</p>
+        </div>
+        <span class="inline-block ml-4 bg-red-400 text-white rounded leading-0 py-0 px-2 hover:cursor-pointer hover:bg-red-600" @click="borrarTarjeta (props.index) ">
+             X 
+        </span>
+    </div>
+</template>
